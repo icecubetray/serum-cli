95b33e1c55211644f80920d05359cc41ff831dc7
icecubetray <icecubetray@users.noreply.github.com>
2019-12-25 03:57:11 +0100

    Add wrapper variants

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include <serum/crypto/hash.h>
+#include <serum/crypto/hashing/debug.h>
 #include <serum/crypto/hashing/md5.h>
 
 
@@ -62,20 +63,46 @@ cmd_md5(int argc, char **argv) {
 		}
 
 		printf("Ident : %X\n", SERUM_MD5);
-		printf("Init  : %p\n", f_init);
-		printf("Clear : %p\n", f_clear);
-		printf("Update: %p\n", f_update);
-		printf("Finish: %p\n", f_finish);
+		printf("Init  : %p\n", (void*)f_init);
+		printf("Clear : %p\n", (void*)f_clear);
+		printf("Update: %p\n", (void*)f_update);
+		printf("Finish: %p\n", (void*)f_finish);
 	} else {
 		unsigned char digest[16];
 
+
+		// Only works for messages up to 63 bytes, too lazy to implement the _update calls
 		struct serum_md5_context md5;
 		serum_md5_init(&md5);
 		serum_md5_finish(&md5, digest, argv[1], strlen(argv[1]), strlen(argv[1]));
 		serum_md5_clear(&md5);
 
-		fputs("Digest: ", stdout);
+		fputs("Digest N: ", stdout);
 		unsigned int i;
+		for (i = 0; i < 16; ++i) {
+			printf("%02X", digest[i]);
+		}
+		puts("");
+
+		// ----------------------------------------------------------------------------------
+
+		struct serum_hash lux;
+		serum_hash_init(&lux, SERUM_MD5);
+		serum_hash_update(&lux, argv[1], strlen(argv[1]));
+		serum_hash_finish(&lux, digest);
+		serum_hash_clear(&lux);
+
+		fputs("Digest L: ", stdout);
+		for (i = 0; i < 16; ++i) {
+			printf("%02X", digest[i]);
+		}
+		puts("");
+
+		// ----------------------------------------------------------------------------------
+
+		serum_hash(SERUM_MD5, digest, argv[1], strlen(argv[1]));
+
+		fputs("Digest U: ", stdout);
 		for (i = 0; i < 16; ++i) {
 			printf("%02X", digest[i]);
 		}
